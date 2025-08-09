b6d37205daefa90715f8746c8ba6c6e0461853a1
Huyenpta <huyenpta1990@gmail.com>
2025-08-09 17:45:35 +0700

    upload file

---
+++ js/demo3.js
@@ -0,0 +1,33 @@
+function f1(){
+    alert("function 1...");
+}
+
+function f2(){
+    alert("function 2....");
+}
+
+function f3(){
+    alert("function 3...");
+}
+
+var ip=document.getElementById("ip");
+var ip2=document.getElementById("ip2");
+var ts=document.getElementById("text-search");
+var ts2=document.getElementById("text-search2");
+function key1(){
+    alert("Function key 1....");
+}
+
+function key2(){
+    var v= ip2.value;
+    ts2.innerText=v;
+}
+
+function key3(){
+    var v= ip.value;
+    ts.innerText=v;
+}
+
+function key4(){
+    alert("Key 4...");
+}
